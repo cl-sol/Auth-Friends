6cea63403c73758e937c03a5ab37c2e6316c5a90
Claudia Sol <Claudia@Claudias-MacBook-Pro.local>
2020-07-13 14:35:00 -0400

    built login form

---
+++ friends/src/components/Login.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+
+function Login() {
+    const [credentials, setCredentials] = useState({
+        id: "",
+        username: "",
+        password: "",
+    });
+    
+    const history = useHistory();
+    const [isLoading, setIsLoading] = useState(false);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setIsLoading(true);
+        return axioswithAuth()
+        .post('api/login', credentials)
+        .then(res => {
+            localStorage.setItem('token', JSON.stringify(res.data.payload));
+            setIsLoading(false);
+            history.push('/protected');
+        })
+        .catch(err => console.log({err: err.message}));
+    }
+
+    function handleChanges(e) {
+        e.persist();
+        setCredentials({...credentials, [e.target.name]: e.target.value})
+    }
+
+    return (
+        <form className="login-form" onSubmit={handleSubmit}>
+            <label htmlFor="username">
+                Username
+            </label>
+            <input
+                type="text"
+                name="username"
+                value={credentials.name}
+                onChange={handleChanges}
+            />
+            <label htmlFor="password">
+                Password
+            </label>
+            <input
+                type="text"
+                name="password"
+                value={credentials.password}
+                onChange={handleChanges}
+            />
+            {isLoading ? "Loading..." : <button className="login-button">Log In</button>}
+        </form>
+    )
+}
